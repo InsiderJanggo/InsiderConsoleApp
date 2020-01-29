4120f8c4678a499f79c24482f88f792ad935e22e
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-01-29 14:03:54 +0700

    added  folder

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InsiderConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
